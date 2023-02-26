3c3d793d0b9233414ddc2f0d74905ab62ce7be87
Cake_fly <cake_fly@pm.me>
2023-02-26 23:36:26 +0200

    bruh

--- v2.py
+++ v2.py
@@ -1,79 +1,78 @@
 import os
 from PIL import Image, ImageDraw, ImageFont
 
-# Получаем список всех файлов в текущей директории
-files = os.listdir()
+# Открываем папку, в которой находится скрипт
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
-# Фильтруем только файлы с расширениями .jpg и .png
-image_files = [file for file in files if file.endswith('.jpg') or file.endswith('.png')]
-
-# Если нет изображений, то завершаем программу
-if not image_files:
-    print("No image files found.")
-    exit()
+# Создаем список файлов с расширением .jpg или .png
+files = [f for f in os.listdir(dir_path) if f.endswith('.jpg') or f.endswith('.png')]
 
 # Определяем размер самого маленького изображения
-min_size = float('inf')
-for file in image_files:
-    with Image.open(file) as img:
-        size = min(img.size)
-        if size < min_size:
-            min_size = size
+sizes = [Image.open(f).size for f in files]
+min_size = min(sizes)
 
-# Размер текста для названия файла
-font_size = int(min_size * 0.05)
+# Создаем новое изображение, на котором будем склеивать оригинальные изображения
+rows = []
+current_row = []
+max_width = min_size[0] * 5  # Максимальная ширина ряда - 5 минимальных изображений
+total_height = 0
+for f in files:
+    # Открываем изображение и изменяем его размер до минимального
+    img = Image.open(f)
+    img = img.resize(min_size)
 
-# Создаем список изображений и их названий
-images = []
-names = []
-for file in image_files:
-    with Image.open(file) as img:
-        # Изменяем размер изображения
-        img = img.resize((min_size, min_size))
+    # Создаем объект ImageDraw для рисования на изображении
+    draw = ImageDraw.Draw(img)
 
-        # Добавляем изображение в список
-        images.append(img)
+    # Определяем размер шрифта для текста с именем файла
+    font_size = int(min_size[1] * 0.05)
+    font = ImageFont.truetype('arial.ttf', font_size)
 
-        # Получаем имя файла без расширения
-        name = os.path.splitext(file)[0]
-        names.append(name)
+    # Рисуем текст с именем файла снизу посередине
+    name = os.path.splitext(os.path.basename(f))[0]  # Имя файла без расширения
+    text_width, text_height = draw.textsize(name, font=font)
+    text_x = (min_size[0] - text_width) // 2
+    text_y = min_size[1] - text_height - font_size
+    draw.text((text_x - 1, text_y - 1), name, font=font, fill='white', stroke_width=2, stroke_fill='white')
+    draw.text((text_x + 1, text_y - 1), name, font=font, fill='white', stroke_width=2, stroke_fill='white')
+    draw.text((text_x + 1, text_y + 1), name, font=font, fill='white', stroke_width=2, stroke_fill='white')
+    draw.text((text_x - 1, text_y + 1), name, font=font, fill='white', stroke_width=2, stroke_fill='white')
+    draw.text((text_x, text_y), name, font=font, fill='black')
 
-# Определяем ширину и высоту выходного изображения
-num_images = len(images)
-num_cols = min(num_images, 5)
-num_rows = (num_images - 1) // num_cols + 1
-out_width = num_cols * min_size
-out_height = num_rows * (min_size + font_size)
+    # Добавляем изображение в текущий ряд
+    current_row.append(img)
+    total_height = max(total_height, img.size[1])
 
-# Создаем выходное изображение
-output = Image.new('RGB', (out_width, out_height), color='white')
+    # Если текущий ряд достиг максимальной ширины, начинаем новый ряд
+    if sum([img.size[0] for img in current_row]) >= max_width:
+        rows.append(current_row)
+        current_row = []
 
-# Создаем объект для рисования на выходном изображении
-draw = ImageDraw.Draw(output)
+# Добавляем последний ряд, если он не пустой
+if current_row:
+    rows.append(current_row)
 
-# Координаты текущей позиции на выходном изображении
+# Определяем общую высоту изображения
+total_height = sum([max([img.size[1] for img in row]) for row in rows])
+
+# Создаем новое изображение, на котором будем склеивать все изображения
+result_img = Image.new('RGB', (max_width, total_height), (255, 255, 255))
+
+#Координаты текущего изображения на результирующем изображении
 x = 0
 y = 0
 
-# Рисуем изображения на выходном изображении
-for i, img in enumerate(images):
-    # Если текущий столбец заполнен, переходим на следующую строку
-    if i % num_cols == 0 and i > 0:
-        x = 0
-        y += min_size + font_size
+# Склеиваем изображения
+result = Image.new('RGB', (max_width, total_height), color='white')
+y_offset = 0
+for row in rows:
+    x_offset = 0
+    row_height = max([img.size[1] for img in row])
+    for img in row:
+        result.paste(img, (x_offset, y_offset))
+        x_offset += img.size[0]
+    y_offset += row_height
 
-    # Вставляем изображение на выходное изображение
-    output.paste(img, (x, y))
+# Сохраняем результат
+result.save('result.png', optimize=True, compression=9)
 
-    # Рисуем название файла под изображением
-    name = names[i]
-    text_width, text_height = draw.textsize(name)
-    text_x = x + min_size // 2 - text_width // 2
-    text_y = y + min_size + font_size // 2 - text_height // 2
-    draw.text((text_x, text_y), name, font=ImageFont.truetype("arial.ttf", font_size), fill='black', stroke_width=2, stroke_fill='white')
-
-    # Переходим к следующему столбцу
-    x += min_size
-
-# Сохраняем выходное изображение
-output.save('output.png', optimize=True, compress_level=9)
